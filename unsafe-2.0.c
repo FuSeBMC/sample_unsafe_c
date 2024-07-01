4469c3d7914c28e476990200a94583517758ef91
Varun <varun.shankar@student.manchester.ac.uk>
2024-07-01 17:12:15 +0100

    new target files and updated yaml

---
+++ unsafe-2.0.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void bufferOverflow() {
+    char buffer[10];
+    printf("Enter some text: ");
+    gets(buffer);
+    printf("you entered: %s\n", buffer);
+}
+
+void formatStringVulnerability() {
+    char userInput[100];
+    printf("Enter a string: ");
+    scanf("%s", userInput);
+    printf(userInput);
+    printf("\n");
+}
+
+void integerOverflow() {
+    unsigned int a, b, result;
+    printf("Enter two numbers: ");
+    scanf("%u %u", &a, &b);
+    result = a + b;
+    printf("The result is: %u\n", result);
+}
+
+int main() {
+    bufferOverflow();
+    formatStringVulnerability();
+    integerOverflow();
+    return 0;
+}
---
+++ unsafe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+    char buffer[10];
+
+    int num = atoi(argv[1]);
+
+    for (int i = 0; i < num; i++) {
+        buffer[i] = 'X';
+    }
+    printf("Buffer contents: %s\n", buffer);
+
+    return 0;
+}
