ce82f88362f4101c3ab652e804a4b51ea2b069bb
varunshankar02 <varun.shankar@student.manchester.ac.uk>
2024-07-08 13:20:27 +0400

    super_duper_safe.c

---
+++ super_duper_safe.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first integer: ");
+    if (scanf("%d", &num1) != 1) {
+        printf("Invalid input. Please enter an integer.\n");
+        return 1;
+    }
+
+    printf("Enter the second integer: ");
+    if (scanf("%d", &num2) != 1) {
+        printf("Invalid input. Please enter an integer.\n");
+        return 1;
+    }
+
+    printf("The sum of %d and %d is %d.\n", num1, num2, num1 + num2);
+
+    return 0;
+}
